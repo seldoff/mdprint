07d164bf9d687562dffd5ea0f885618b1f21845b
Alex <alex@seldoff.me>
2023-05-27 02:33:41 +0200

    Cleanup

--- tests/sanitize.test.ts
+++
@@ -1,8 +0,0 @@
-import fs from "node:fs/promises"
-import DOMPurify from "dompurify"
-
-test("sanitize", async () => {
-  const notSanitized = await fs.readFile("tests/all_features.html", { encoding: "utf-8" })
-  const actual = DOMPurify.sanitize(notSanitized)
-  expect(actual).toMatchSnapshot()
-})
