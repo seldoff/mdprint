ab568f97aafa317f0745871c74c2e1a0a209a870
Alex <alex@seldoff.me>
2023-05-27 02:33:53 +0200

    Add a few more GH styles

--- src/htmlTemplate.ts
+++ src/htmlTemplate.ts
@@ -1,6 +1,8 @@
 // node_modules/@primer/css/index.scss
 
 // @ts-ignore
+import mdCss from "!!raw-loader!./md.css"
+// @ts-ignore
 import template from "!!raw-loader!./template.html"
 // @ts-ignore
 import colorModes from "!!raw-loader!@primer/css/dist/color-modes.css"
@@ -9,10 +11,12 @@ import core from "!!raw-loader!@primer/css/dist/core.css"
 // @ts-ignore
 import product from "!!raw-loader!@primer/css/dist/product.css"
 // @ts-ignore
+import syntax from "!!raw-loader!github-syntax-light/lib/github-light.css"
+// @ts-ignore
 import iframeResizer from "!!raw-loader!iframe-resizer/js/iframeResizer.contentWindow.min.js"
 
 function getMdStyles(): string {
-  const combined = [colorModes, core, product].join("\n")
+  const combined = [colorModes, core, product, syntax, mdCss].join("\n")
   return `<style>${combined}</style>`
 }
 
