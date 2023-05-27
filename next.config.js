321831707ddf31c93a44d7d172a5adc34ba9cee9
Alex <alex@seldoff.me>
2023-05-27 03:32:01 +0200

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
